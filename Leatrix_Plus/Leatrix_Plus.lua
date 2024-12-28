99997e775fef8bda902befda97b0c7fd438fd19c
Dololo <class2u@gmail.com>
2024-12-28 10:49:21 +0800

    更新 功能百寶箱 4.0.40
    # Conflicts:
    #	Leatrix_Plus/Leatrix_Plus.toc
    #	Leatrix_Plus/Leatrix_Plus_Locale.lua
    #	Leatrix_Plus/Vanilla/Leatrix_Plus_Locale.lua

--- Leatrix_Plus/Leatrix_Plus.lua
+++ Leatrix_Plus/Leatrix_Plus.lua
@@ -1,5 +1,5 @@
 ﻿----------------------------------------------------------------------
--- 	Leatrix Plus 4.0.39 (18th December 2024)
+-- 	Leatrix Plus 4.0.40 (25th December 2024)
 ----------------------------------------------------------------------
 
 --	01:Functions  02:Locks    03:Restart  40:Player   45:Rest
@@ -19,7 +19,7 @@
 	local void
 
 	-- Version
-	LeaPlusLC["AddonVer"] = "4.0.39"
+	LeaPlusLC["AddonVer"] = "4.0.40"
 
 	-- Get locale table
 	local void, Leatrix_Plus = ...
