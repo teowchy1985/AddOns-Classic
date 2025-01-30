6997c78978674f68c66282be7f7cde183d6bf607
Dololo <class2u@gmail.com>
2025-01-30 11:07:40 +0800

    更新 世界地圖增強 4.0.45

--- Leatrix_Maps/Vanilla/Leatrix_Maps.lua
+++ Leatrix_Maps/Vanilla/Leatrix_Maps.lua
@@ -1,6 +1,6 @@
 ﻿
 	----------------------------------------------------------------------
-	-- 	Leatrix Maps 1.15.72 (22nd January 2025)
+	-- 	Leatrix Maps 1.15.73 (29th January 2025)
 	----------------------------------------------------------------------
 
 	-- 10:Func, 20:Comm, 30:Evnt, 40:Panl
@@ -12,7 +12,7 @@
 	local LeaMapsLC, LeaMapsCB, LeaDropList, LeaConfigList, LeaLockList = {}, {}, {}, {}, {}
 
 	-- Version
-	LeaMapsLC["AddonVer"] = "1.15.72"
+	LeaMapsLC["AddonVer"] = "1.15.73"
 
 	-- Get locale table
 	local void, Leatrix_Maps = ...
