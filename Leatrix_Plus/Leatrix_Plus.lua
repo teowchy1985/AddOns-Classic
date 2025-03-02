76aa1b76abdbfcec21f193874ad33b89bb53aaf5
Dololo <class2u@gmail.com>
2025-03-02 12:33:46 +0800

    更新 功能百寶箱 4.0.49

--- Leatrix_Plus/Leatrix_Plus.lua
+++ Leatrix_Plus/Leatrix_Plus.lua
@@ -1,5 +1,5 @@
 ﻿----------------------------------------------------------------------
--- 	Leatrix Plus 4.0.48 (21st February 2025)
+-- 	Leatrix Plus 4.0.49 (28th February 2025)
 ----------------------------------------------------------------------
 
 --	01:Functions  02:Locks    03:Restart  40:Player   45:Rest
@@ -19,7 +19,7 @@
 	local void
 
 	-- Version
-	LeaPlusLC["AddonVer"] = "4.0.48"
+	LeaPlusLC["AddonVer"] = "4.0.49"
 
 	-- Get locale table
 	local void, Leatrix_Plus = ...
