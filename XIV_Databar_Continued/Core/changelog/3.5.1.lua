f948e75426bc2dbd308070c9bae9eaf137c38987
Dololo <class2u@gmail.com>
2025-03-09 10:48:46 +0800

    更新 功能資訊列 3.5.1

---
+++ XIV_Databar_Continued/Core/changelog/3.5.1.lua
@@ -0,0 +1,42 @@
+local AddOnName, XIVBar = ...;
+
+XIVBar.Changelog[3510] = {
+    version_string = "3.5.1",
+    release_date = "2025/03/08",
+    important = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    new = {
+        ["zhCN"] = {},
+        ["zhTW"] = {
+            "[正式服] 重新製作 [M+] 功能，讓 [當前賽季] 更容易維護和動態使用賽季開始日期。",
+        },
+        ["enUS"] = {
+            "[Retail] Rework of the [Mythic+] functionnality to make the [Current Season] easier to maintain and dynamic using season start dates.",
+        },
+        ["frFR"] = {
+            "[Retail] Refonte de la fonctionnalité [Mythique+] pour rendre la partie [Saison actuelle] plus facile à maintenir et dynamique en utilisant les dates de debut de saison.",
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    improvment = {
+        ["zhCN"] = {},
+        ["zhTW"] = {
+            "[正式服] 修正 [M+] 功能中的字母排序。",
+        },
+        ["enUS"] = {
+            "[Retail] Fixed the alphabetical ordering of dungeons in the [Mythic+] functionnality.",
+        },
+        ["frFR"] = {
+            "[Retail] Correction de l'ordre alphabétique des donjons dans la fonctionnalité [Mythique+].",
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    }
+}
