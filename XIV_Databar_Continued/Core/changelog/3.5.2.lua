14c3aa625b593fa41682d21603338679bcbe40e2
Dololo <class2u@gmail.com>
2025-03-18 09:35:13 +0800

    更新 功能資訊列 3.5.2

---
+++ XIV_Databar_Continued/Core/changelog/3.5.2.lua
@@ -0,0 +1,45 @@
+local AddOnName, XIVBar = ...;
+
+XIVBar.Changelog[3520] = {
+    version_string = "3.5.2",
+    release_date = "2025/03/18",
+    important = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    new = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    improvment = {
+        ["zhCN"] = {},
+        ["zhTW"] = {
+            "[正式服] 修正 [微型選單] 社交彈出資訊的活動圖示。",
+            "[正式服] 修正 [旅行傳送] 模組不會顯示傳送彈出資訊。",
+            "[正式服] 修正 [旅行傳送] 模組中 M+ 傳送彈出資訊的位置，會根據資訊列在畫面上的位置動態調整。",
+            "[正式服] 暫時更改法術書和天賦 [微型選單] 按鈕 API。",
+        },
+        ["enUS"] = {
+            "[Retail] Fixed activity icons in the [Micromenu] social popup.",
+            "[Retail] Fixed the Teleports popup not showing in the [Travel] module.",
+            "[Retail] Fixed the Mythic+ Teleports popup positioning in the [Travel] module to make it dynamic depending on the bar position on screen.",
+            "[Retail] Tentative change to the Spellbook and Talents [Micromenu] buttons APIs.",
+        },
+        ["frFR"] = {
+            "[Retail] Correction des icônes des activités dans la popup sociale du module [Micro menu] .",
+            "[Retail] Correction de la popup de sélection de téléportation alternative qui n'était plus affichée dans le module [Voyage] .",
+            "[Retail] Correction du positionnement de la popup de téléportation mythique+ du module [Voyage] pour qu'elle soit dynamique en fonction de la position de la barre sur l'écran.",
+            "[Retail] Tentative de modification API des boutons du Grimoire et des Talents dans le module [Micro menu].",
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    }
+}
