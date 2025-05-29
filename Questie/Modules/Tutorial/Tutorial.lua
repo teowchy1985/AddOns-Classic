ede1bc67153398549eca26b11f9c956092056a3b
teowchy1985 <147220163+teowchy1985@users.noreply.github.com>
2025-05-30 00:46:29 +0800

    Update Tutorial.lua

--- Questie/Modules/Tutorial/Tutorial.lua
+++ Questie/Modules/Tutorial/Tutorial.lua
@@ -1,27 +1,16 @@
 ---@class Tutorial
 local Tutorial = QuestieLoader:CreateModule("Tutorial")
 
----@type QuestieCombatQueue
-local QuestieCombatQueue = QuestieLoader:ImportModule("QuestieCombatQueue")
----@type Expansions
-local Expansions = QuestieLoader:ImportModule("Expansions")
-
 function Tutorial.Initialize()
-    if (Expansions.Current >= Expansions.Wotlk) and GetCVar("questPOI") ~= nil and (not Questie.db.global.tutorialObjectiveTypeChosen) then
-        QuestieCombatQueue:Queue(function()
-            Tutorial.CreateChooseObjectiveTypeFrame()
-        end)
+    if (Questie.IsWotlk or Questie.IsCata) and GetCVar("questPOI") ~= nil and (not Questie.db.global.tutorialObjectiveTypeChosen) then
+        Tutorial.CreateChooseObjectiveTypeFrame()
     end
 
     if Questie.IsSoD and (not Questie.db.profile.tutorialShowRunesDone) then
-        QuestieCombatQueue:Queue(function()
-            Tutorial.ShowRunes()
-        end)
+        Tutorial.ShowRunes()
     end
 
     if (not Questie.db.profile.tutorialRejectInBattlegroundsDone) then
-        QuestieCombatQueue:Queue(function()
-            Tutorial.AutoRejectInBattlegroundsFrame()
-        end)
+        Tutorial.AutoRejectInBattlegroundsFrame()
     end
 end
