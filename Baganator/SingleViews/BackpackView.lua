aa095c09fc621b2cc1eda5f0641dc9f69f397737
teowchy1985 <147220163+teowchy1985@users.noreply.github.com>
2025-05-29 23:37:44 +0800

    Update BackpackView.lua

--- Baganator/SingleViews/BackpackView.lua
+++ Baganator/SingleViews/BackpackView.lua
@@ -1,5 +1,4 @@
----@class addonTableBaganator
-local addonTable = select(2, ...)
+local _, addonTable = ...
 
 BaganatorSingleViewBackpackViewMixin = CreateFromMixins(BaganatorItemViewCommonBackpackViewMixin)
 
