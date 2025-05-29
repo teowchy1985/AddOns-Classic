89822a0a3e20834c8316516af2cbe3d39fbd4dae
teowchy1985 <147220163+teowchy1985@users.noreply.github.com>
2025-05-30 00:35:23 +0800

    Update Leatrix_Plus_Library.lua

--- Leatrix_Plus/Leatrix_Plus_Library.lua
+++ Leatrix_Plus/Leatrix_Plus_Library.lua
@@ -1,5 +1,5 @@
 ﻿----------------------------------------------------------------------
--- L00: Leatrix Plus Library for Mists of Pandaria Classic
+-- L00: Leatrix Plus Library for Cataclysm Classic
 ----------------------------------------------------------------------
 
 -- LibDBIcon 10.0.1:
