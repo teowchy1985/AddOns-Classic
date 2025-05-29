89cd6e9e8e0aad3baf2871083d8f30429275aef9
teowchy1985 <147220163+teowchy1985@users.noreply.github.com>
2025-05-29 23:38:07 +0800

    Update BagUsageChecks.lua

--- Baganator/Sorting/BagUsageChecks.lua
+++ Baganator/Sorting/BagUsageChecks.lua
@@ -1,5 +1,4 @@
----@class addonTableBaganator
-local addonTable = select(2, ...)
+local _, addonTable = ...
 -- Check the sort priority (so special bags get filled up first) and
 -- corresponding bag contents checks (e.g. reagents only) if any
 function addonTable.Sorting.GetBagUsageChecks(bagIDs)
