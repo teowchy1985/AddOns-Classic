8ae5dc6726d4ee9cb72fdfa5bb0698eb457134f0
teowchy1985 <147220163+teowchy1985@users.noreply.github.com>
2025-05-29 23:37:28 +0800

    Update Pools.lua

--- Baganator/ItemViewCommon/Pools.lua
+++ Baganator/ItemViewCommon/Pools.lua
@@ -1,12 +1,11 @@
----@class addonTableBaganator
-local addonTable = select(2, ...)
+local _, addonTable = ...
 local classicCachedObjectCounter = 0
 
 function addonTable.ItemViewCommon.GetCachedItemButtonPool(self)
   if addonTable.Constants.IsRetail then
     return CreateFramePool("ItemButton", self, "BaganatorRetailCachedItemButtonTemplate", nil, false, function(b) b:UpdateTextures() end)
   else
-    return CreateObjectPool(function()
+    return CreateObjectPool(function(pool)
       classicCachedObjectCounter = classicCachedObjectCounter + 1
       local b = CreateFrame("Button", "BGRCachedItemButton" .. classicCachedObjectCounter, self, "BaganatorClassicCachedItemButtonTemplate")
       b:UpdateTextures()
@@ -19,7 +18,7 @@ function addonTable.ItemViewCommon.GetLiveItemButtonPool(self)
   if addonTable.Constants.IsRetail then
     return CreateFramePool("ItemButton", self, "BaganatorRetailLiveContainerItemButtonTemplate", nil, false, function(b) b:UpdateTextures() end)
   else
-    return CreateObjectPool(function()
+    return CreateObjectPool(function(pool)
       classicCachedObjectCounter = classicCachedObjectCounter + 1
       local b = CreateFrame("Button", "BGRLiveItemButton" .. classicCachedObjectCounter, self, "BaganatorClassicLiveContainerItemButtonTemplate")
       b:UpdateTextures()
@@ -32,7 +31,7 @@ function addonTable.ItemViewCommon.GetLiveGuildItemButtonPool(parent)
   if addonTable.Constants.IsRetail then
     return CreateFramePool("ItemButton", parent, "BaganatorRetailLiveGuildItemButtonTemplate", nil, false, function(b) b:UpdateTextures() end)
   else
-    return CreateObjectPool(function()
+    return CreateObjectPool(function(pool)
       classicCachedObjectCounter = classicCachedObjectCounter + 1
       local b = CreateFrame("Button", "BGRLiveItemButton" .. classicCachedObjectCounter, parent, "BaganatorClassicLiveGuildItemButtonTemplate")
       b:UpdateTextures()
@@ -45,7 +44,7 @@ function addonTable.ItemViewCommon.GetTabButtonPool(parent)
   if addonTable.Constants.IsRetail then
     return CreateFramePool("Button", parent, "BaganatorRetailTabButtonTemplate")
   else
-    return CreateObjectPool(function()
+    return CreateObjectPool(function(pool)
       classicCachedObjectCounter = classicCachedObjectCounter + 1
       return CreateFrame("Button", "BGRItemViewCommonTabButton" .. classicCachedObjectCounter, parent, "BaganatorClassicTabButtonTemplate")
     end, FramePool_HideAndClearAnchors or Pool_HideAndClearAnchors)
@@ -53,7 +52,7 @@ function addonTable.ItemViewCommon.GetTabButtonPool(parent)
 end
 
 function addonTable.ItemViewCommon.GetSideTabButtonPool(parent)
-  return CreateObjectPool(function()
+  return CreateObjectPool(function(pool)
     classicCachedObjectCounter = classicCachedObjectCounter + 1
     return CreateFrame("Button", "BGRItemViewCommonTabButton" .. classicCachedObjectCounter, parent, "BaganatorRightSideTabButtonTemplate")
   end, FramePool_HideAndClearAnchors or Pool_HideAndClearAnchors)
