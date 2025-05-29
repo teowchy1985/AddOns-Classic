88fadd397081a661744022769f45e53e89e5ab07
teowchy1985 <147220163+teowchy1985@users.noreply.github.com>
2025-05-30 00:35:58 +0800

    Update Companion.lua

--- MinimalArchaeology/addon/components/Companion.lua
+++ MinimalArchaeology/addon/components/Companion.lua
@@ -100,13 +100,8 @@ local function InitDistanceTracker()
     Companion.trackerFrame:SetScript("OnEnter", function(self)
         if (MinArch.db.profile.companion.showHelpTip) then
             HelpPlate_TooltipHide();
-            if MINARCH_EXPANSION == 'Cata' then
-                HelpPlateTooltip.ArrowUP:Show();
-                HelpPlateTooltip.ArrowGlowUP:Show();
-            else
-                HelpPlateTooltip.ArrowUp:Show();
-                HelpPlateTooltip.ArrowGlowUp:Show();
-            end
+            HelpPlateTooltip.ArrowUP:Show();
+            HelpPlateTooltip.ArrowGlowUP:Show();
             HelpPlateTooltip:SetPoint("BOTTOM", Companion.frame, "TOP", 0, 20);
             HelpPlateTooltip.Text:SetText(L["COMPANION_TUTORIAL_1"]);
             HelpPlateTooltip:Show();
