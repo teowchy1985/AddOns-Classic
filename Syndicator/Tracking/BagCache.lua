ca3d232e0c6c413eb19566e9834d3a48eac2288d
teowchy1985 <147220163+teowchy1985@users.noreply.github.com>
2025-05-30 00:49:54 +0800

    Update BagCache.lua

--- Syndicator/Tracking/BagCache.lua
+++ Syndicator/Tracking/BagCache.lua
@@ -89,7 +89,7 @@ function SyndicatorBagCacheMixin:OnEvent(eventName, ...)
       self.pending.bags[bagID] = true
     elseif bankBags[bagID] and self.bankOpen then
       self.pending.bank[bagID] = true
-    elseif warbandBags[bagID] and self.bankOpen and C_PlayerInfo.HasAccountInventoryLock() then
+    elseif warbandBags[bagID] and self.bankOpen then
       self.pending.warband[bagID] = true
     end
     self:QueueCaching()
@@ -129,11 +129,10 @@ function SyndicatorBagCacheMixin:OnEvent(eventName, ...)
 
   -- Guessing that these events may be fired when a new warband tab is purchased
   elseif eventName == "BANK_TABS_CHANGED" or eventName == "PLAYER_ACCOUNT_BANK_TAB_SLOTS_CHANGED" then
-    if (self.bankOpen or time() - self.craftingTime < craftingItemUpdateDelay) and C_PlayerInfo.HasAccountInventoryLock() then
+    if self.bankOpen or time() - self.craftingTime < craftingItemUpdateDelay then
       self:ScanWarbandSlots()
-      local tab = ...
-      if tab then
-        self.pending.warband[Syndicator.Constants.AllWarbandIndexes[tab]] = true
+      for bagID in pairs(warbandBags) do
+        self.pending.warband[bagID] = true
       end
       self:QueueCaching()
     end
@@ -207,7 +206,7 @@ function SyndicatorBagCacheMixin:UpdateContainerSlots()
 end
 
 function SyndicatorBagCacheMixin:ScanWarbandSlots()
-  if C_Bank == nil or C_Bank.FetchPurchasedBankTabData == nil or not C_PlayerInfo.HasAccountInventoryLock() then
+  if C_Bank == nil or C_Bank.FetchPurchasedBankTabData == nil then
     return
   end
 
