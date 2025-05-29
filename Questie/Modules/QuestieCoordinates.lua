3dea8734ba7eadfbfa876fc59baa0effb08f5ed8
teowchy1985 <147220163+teowchy1985@users.noreply.github.com>
2025-05-30 00:45:15 +0800

    Update QuestieCoordinates.lua

--- Questie/Modules/QuestieCoordinates.lua
+++ Questie/Modules/QuestieCoordinates.lua
@@ -17,8 +17,6 @@ local GetMinimapZoneText = GetMinimapZoneText;
 local IsInInstance = IsInInstance;
 local format = format;
 
-local IsAddOnLoaded = C_AddOns.IsAddOnLoaded or IsAddOnLoaded
-
 
 local function GetMapTitleText()
     local regions = {WorldMapFrame.BorderFrame:GetRegions()}
